848cad205e7a1c1bbb4fd2fd895176336c705d42
sjkfeel <layum@naver.com>
2025-01-17 12:51:31 +0900

    java 문자

--- mandarin.java/java_study1/src/study0116/test.java
+++ mandarin.java/java_study1/src/study0116/test.java
@@ -25,8 +25,78 @@ public class test {
         age = 27;
         age = '가'; // 자바에서 '' 표현은 문자를 의믜
                    // "" 표현은 문자열을 의미
+        System.out.println(27);
+        System.out.println(27);
+        byte num1 = 120;
+        System.out.println(num1);
+//      num1 = 130;
+        int num2 = 20; // 변수를 선언하면서 데이터 저장 -변수 초기화
+        int num3 = 43;
+        // num3은 43이다. 라고 하면 안됨 num3과 43이 같다는 표현이라
+        // num3에 43을 저장했다 , num3에 43을 대힙했다. 라고 표현
         
+        int sum = num2+num3;
+        System.out.println( sum );
         
-        }
-
+        // 사각형의 너비와 높이 값으로 넓이를 구하세요
+        
+       
+//        int width;
+//        int height;
+//        width =20;
+//        height =32;
+//        int area = width * height;
+//        System.out.println( area );
+        
+          int blue = 20;
+          int red = 15;
+          int white = 0;
+          // 파란컵에 오렌지주스가 20ml 있다. 
+          // 빨간컵에 딸기주스가 15ml 있다.
+          // 파란컵에 딸기주스를, 빨간컵에 오렌지주스를 담으려면 어떻게 해야하는가?
+          // blue와 red 값 출력
+          // blue=15, red=20 이 방법은 아님
+          white = blue;
+          blue = red;
+          red = white;
+          System.out.println( blue );
+          System.out.println( red );
+          
+          int birthYear = 1991;
+          // 김철수가 태어난 년도를 birth_year에 저장했다.
+          // 나이는?
+          int myAge = 2025 - birthYear;
+          System.out.println( myAge );
+          
+          // 변수란 데이터(값)을 저장할 수 있는 메모리 공간
+          // 변수의 공간에는 데이터타입에 맞는 데이터 만 저장 할 수 있게 해줘야 한다.
+          // 변수의 선언은 데이터 타입 변수명 (예-int num;)
+          // 변수 이름은 카멜 표기법이 일반적이다
+          // 변수 이름은 변수의 사용 목적에 맞는 이름으로 한다.(길어도 됨)
+          
+          float fnum = 2.156f;
+          System.out.println( fnum );
+          fnum = 2.123456789f;
+          System.out.println( fnum );
+          double dnum = 1.1234567890123456289;
+          System.out.println(dnum);
+          
+          char ch='a';
+          System.out.println(ch);
+          ch='가';
+          System.out.println(ch);
+          ch= 97 ;
+          System.out.println(ch);
+          ch='H';
+          System.out.println(ch - 'A');
+          System.out.println( ch- 64 );
+          String name="홍길동"; //문자열은 string 클래스 타입 사용
+          
+          boolean isApple = true; // 참, 거짓 값을 가지는 타입
+          System.out.println( isApple );
+        		 
+        
+    
+	}
 }
+
---
+++ mandarin.java/java_study1/src/study0117/Javatype.java
@@ -0,0 +1,15 @@
+package study0117;
+
+public class Javatype {
+
+	public static void main(String[] args) {
+		
+		int num=20;
+		float pi=3.14f;
+		
+		pi = num;   // 자동 형변환
+		num = (int)pi;  //강제 형변환 - 일시적 타입 변환
+
+	}
+
+}
